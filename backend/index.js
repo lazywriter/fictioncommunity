ce747ac6b7ac26258589171417958c55d8ca3f4a
lazywriter <48875544+lazywriter@users.noreply.github.com>
2024-07-01 17:31:10 +0530

    Second Phase Complete

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const cors = require("cors");
 const mongoDB = require("./db");
 
 app.use((req, res, next) => {
@@ -12,12 +13,21 @@ app.use((req, res, next) => {
   next();
 });
 
+const corsOptions = {
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+
 mongoDB();
 
 app.use(express.json());
 app.use("/api", require("./routes/CreateUser"));
 app.use("/api", require("./routes/DisplayData"));
 app.use("/api", require("./routes/CreateBook"));
+app.use("/api", require("./routes/UserData"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
--- src/screens/Account.js
+++ src/screens/Account.js
@@ -1,19 +1,106 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import { Link } from "react-router-dom";
 
 function Account() {
+  const [user, setuser] = useState({});
+
+  const loadData = async () => {
+    const token = localStorage.getItem("authToken");
+    let response = await fetch("http://localhost:5000/api/me", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+    response = await response.json();
+    setuser(response);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
     <div>
       <Navbar></Navbar>
-      <p>This is Accounts page</p>
-      <Link
-        to="/addbook"
-        className="btn btn-primary"
-        style={{ borderRadius: "40%" }}
-      >
-        +
-      </Link>
+      <div>
+        <section className="vh-100">
+          <div className="container py-5 h-100">
+            <div className="row d-flex justify-content-center align-items-center h-100">
+              <div className="col-md-12 col-xl-4">
+                <div className="card" style={{ borderRadius: "15px" }}>
+                  <div className="card-body text-center">
+                    <div className="mt-3 mb-4">
+                      <img
+                        src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava2-bg.webp"
+                        className="rounded-circle img-fluid"
+                        style={{ width: "100px" }}
+                      />
+                    </div>
+                    <h4 className="mb-2">{user.name}</h4>
+                    <p className="text-muted mb-4">
+                      {user.email} <span className="mx-2">|</span>{" "}
+                    </p>
+                    <div className="mb-4 pb-2">
+                      <button
+                        type="button"
+                        data-mdb-button-init
+                        data-mdb-ripple-init
+                        className="btn btn-outline-primary btn-floating"
+                      >
+                        <i className="fab fa-facebook-f fa-lg"></i>
+                      </button>
+                      <button
+                        type="button"
+                        data-mdb-button-init
+                        data-mdb-ripple-init
+                        className="btn btn-outline-primary btn-floating"
+                      >
+                        <i className="fab fa-twitter fa-lg"></i>
+                      </button>
+                      <button
+                        type="button"
+                        data-mdb-button-init
+                        data-mdb-ripple-init
+                        className="btn btn-outline-primary btn-floating"
+                      >
+                        <i className="fab fa-skype fa-lg"></i>
+                      </button>
+                    </div>
+                    <Link to="/addbook">
+                      <button
+                        type="button"
+                        data-mdb-button-init
+                        data-mdb-ripple-init
+                        className="btn btn-primary btn-rounded btn-lg"
+                      >
+                        Add Books for Lending
+                      </button>
+                    </Link>
+
+                    <div className="d-flex justify-content-between text-center mt-5 mb-2">
+                      <div>
+                        <p className="mb-2 h5">8471</p>
+                        <p className="text-muted mb-0">Wallets Balance</p>
+                      </div>
+                      <div className="px-3">
+                        <p className="mb-2 h5">8512</p>
+                        <p className="text-muted mb-0">Income amounts</p>
+                      </div>
+                      <div>
+                        <p className="mb-2 h5">4751</p>
+                        <p className="text-muted mb-0">Total Transactions</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
