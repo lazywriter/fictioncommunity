5b5453a0f558a6bf50269b9743b7776ab54bc999
lazywriter <48875544+lazywriter@users.noreply.github.com>
2024-07-03 00:01:06 +0530

    Added the Uploaded by You feature

--- src/screens/Account.js
+++ src/screens/Account.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import UserProfile from "../components/UserProfile";
+import Card from "../components/Card";
 
 function Account() {
   const [user, setuser] = useState({});
+  const [bookData, setbookData] = useState([]);
 
   const loadData = async () => {
     const token = localStorage.getItem("authToken");
@@ -18,8 +20,20 @@ function Account() {
     setuser(response);
   };
 
+  const loadBooks = async () => {
+    let response = await fetch("http://localhost:5000/api/bookData", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    response = await response.json();
+    setbookData(response[0]);
+  };
+
   useEffect(() => {
     loadData();
+    loadBooks();
   }, []);
 
   return (
@@ -27,6 +41,22 @@ function Account() {
       <Navbar></Navbar>
       <UserProfile userData={user}></UserProfile>
       <hr />
+      <div className="container">
+        <h1 className="mb-2 lead">Books Being Lent By You :</h1>
+        <div className="row mb-3">
+          {bookData.length !== 0
+            ? bookData
+                .filter((book) => book.lenderEmail === user.email)
+                .map((filterItems) => {
+                  return (
+                    <div className="col-12 col-md-6 col-lg-3">
+                      <Card bookData={filterItems}></Card>
+                    </div>
+                  );
+                })
+            : ""}
+        </div>
+      </div>
     </div>
   );
 }
